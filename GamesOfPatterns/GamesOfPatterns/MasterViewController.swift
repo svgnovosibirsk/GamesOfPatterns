8cb9aaf2d2b3f44791321f3c141778a67f22a12b
Sergey <>
2023-07-30 12:38:41 +0400

    refactor setupTableView

--- GamesOfPatterns/GamesOfPatterns/MasterViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/MasterViewController.swift
@@ -9,43 +9,46 @@ import UIKit
 
 class MasterViewController: UIViewController {
     let patterns = ["First", "Second", "Third"]
+    let cellId = "patternCell"
+    
     let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemMint // TEST
+        view.backgroundColor = .white
         
         tableView.delegate = self
         tableView.dataSource = self
         
         setupTableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
     func setupTableView() {
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
     }
     
 }
 
 extension MasterViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        1 // TEST
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        patterns.count
+        patterns.count // TEST
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         cell.textLabel?.text = patterns[indexPath.row]
-        cell.imageView?.image = UIImage(systemName: "star.fill") // TEST
+        //cell.imageView?.image = UIImage(systemName: "star.fill") // TEST
         
         return cell
     }
