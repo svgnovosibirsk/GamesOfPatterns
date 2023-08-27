7e548ab44ebda263f25e1f2439930aa3cad09539
Sergey <>
2023-08-27 09:56:54 +0400

    add name label

--- GamesOfPatterns/GamesOfPatterns/AbstractFactoryScreen/View/AbstractFactoryViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/AbstractFactoryScreen/View/AbstractFactoryViewController.swift
@@ -11,6 +11,8 @@ final class AbstractFactoryViewController: UIViewController {
     
     let presenter = AbstractFactoryPresenter()
     
+    let nameLabel = UILabel()
+    
     var segmentedControl: UISegmentedControl! = nil
    
     override func viewDidLoad() {
@@ -27,7 +29,9 @@ final class AbstractFactoryViewController: UIViewController {
         segmentedControl.backgroundColor = .systemGreen
         segmentedControl.selectedSegmentTintColor = .green
         
+        setupNameLabel()
         setSegmentedControlConstraints()
+        
     }
     
     private func setSegmentedControlConstraints() {
@@ -38,6 +42,26 @@ final class AbstractFactoryViewController: UIViewController {
             segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
             segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
             segmentedControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        ])
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.text = "TMNT"
+        nameLabel.textAlignment = .center
+        nameLabel.textColor = .systemGreen
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        
+        setNameLabelConstraints()
+    }
+    
+    private func setNameLabelConstraints() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5)
         ])
     }
 
