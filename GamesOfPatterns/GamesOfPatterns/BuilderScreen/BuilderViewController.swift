c4ddf9731bf41a694c68b765c789a1bdc94df6e0
Sergey <>
2023-09-21 09:53:18 +0400

    add switch

--- GamesOfPatterns/GamesOfPatterns/BuilderScreen/BuilderViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/BuilderScreen/BuilderViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class BuilderViewController: UIViewController {
     let backgroundView = UIImageView(frame: .zero)
+    let sideSwitch = UISwitch(frame: .zero)
     
     override func viewDidLoad() {
         title = "Builder"
@@ -18,8 +19,11 @@ final class BuilderViewController: UIViewController {
     
     private func setupScreen() {
         setupBackgroundView()
+        
+        setupSideSwitch()
     }
     
+    // MARK: Background
     private func setupBackgroundView() {
         backgroundView.image = ImagesProvider.swBackgroundBlack
         backgroundView.contentMode = .scaleAspectFill
@@ -38,4 +42,40 @@ final class BuilderViewController: UIViewController {
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+    
+    // MARK: Switch
+    private func setupSideSwitch() {
+        sideSwitch.addTarget(self, action: #selector(self.switchStateDidChange(_:)), for: .valueChanged)
+        
+        sideSwitch.setOn(true, animated: false)
+        
+        sideSwitch.thumbTintColor = .systemGray
+        sideSwitch.onTintColor = .black
+        sideSwitch.tintColor = .white
+        sideSwitch.layer.cornerRadius = sideSwitch.frame.height / 2.0
+        sideSwitch.backgroundColor = .white
+        sideSwitch.clipsToBounds = true
+        
+        
+        setupSideSwitchConstraints()
+    }
+    
+    private func setupSideSwitchConstraints() {
+        sideSwitch.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(sideSwitch)
+        
+        NSLayoutConstraint.activate([
+            sideSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            sideSwitch.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+        ])
+    }
+
+    @objc func switchStateDidChange(_ sender:UISwitch!)
+     {
+         if (sender.isOn == true) {
+             print("UISwitch state is now ON")
+         } else {
+             print("UISwitch state is now Off")
+         }
+     }
 }
