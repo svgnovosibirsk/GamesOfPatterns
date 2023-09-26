b64cd8acea4f51f5d3acc59877ea7366a8fe1841
Sergey <>
2023-09-26 11:18:55 +0400

    setup ui

--- GamesOfPatterns/GamesOfPatterns/Protorype/PrototypeViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/Protorype/PrototypeViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 final class PrototypeViewController: UIViewController {
     
-    var backgroundView = UIImageView(frame: .zero)
-    var stackView = UIStackView()
+    let backgroundView = UIImageView(frame: .zero)
+    let stackView = UIStackView(frame: .zero)
+    let topLabel = UILabel(frame: .zero)
+    let numberLabel = UILabel(frame: .zero)
+    let smithImageView = UIImageView(frame: .zero)
+    let cloneButton = UIButton(frame: .zero)
+    let spacerView = UILabel(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,11 @@ final class PrototypeViewController: UIViewController {
     private func setupScreen() {
         setupBackgroundView()
         setupStackView()
+        setupTopLabel()
+        setupNumberLabel()
+        setupSmithImageView()
+        setupSpacerView()
+        setupCloneButton()
     }
     
     // MARK: Background
@@ -47,7 +57,11 @@ final class PrototypeViewController: UIViewController {
     
     // MARK: Stackview
     private func setupStackView() {
-        stackView.backgroundColor = .systemCyan // test
+        stackView.backgroundColor = UIColor(white: 0, alpha: 0.2)
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 20
         
         setupStackViewConstraints()
     }
@@ -57,10 +71,60 @@ final class PrototypeViewController: UIViewController {
         view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    // MARK: Top Label
+    private func setupTopLabel() {
+        topLabel.text = "Number of Smiths"
+        topLabel.textColor = .green
+        topLabel.font = UIFont.boldSystemFont(ofSize: 30.0)
+        stackView.addArrangedSubview(topLabel)
+    }
+    
+    // MARK: Number Label
+    private func setupNumberLabel() {
+        numberLabel.text = "1"
+        numberLabel.textColor = .green
+        numberLabel.font = UIFont.boldSystemFont(ofSize: 80.0)
+        stackView.addArrangedSubview(numberLabel)
+    }
+    
+    // MARK: Smith Image View
+    private func setupSmithImageView() {
+        smithImageView.image = ImagesProvider.mxSmith1
+        smithImageView.layer.cornerRadius = 20
+        smithImageView.layer.masksToBounds = true
+        stackView.addArrangedSubview(smithImageView)
+    }
+    
+    // MARK: Clone Button
+    private func setupCloneButton() {
+        cloneButton.setTitle("     Clone     ", for: .normal)
+        cloneButton.setTitleColor(.green, for: .normal)
+        cloneButton.setTitleColor(.systemGreen, for: .highlighted)
+        cloneButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30.0)
+        cloneButton.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        cloneButton.layer.cornerRadius = 10
+        stackView.addArrangedSubview(cloneButton)
+    }
+    
+    // MARK: Spacer View
+    private func setupSpacerView() {
+        spacerView.numberOfLines = 0
+        spacerView.text = """
+        
+        
+        
+        
+        
+        """
+        spacerView.textColor = .white
+        spacerView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        stackView.addArrangedSubview(spacerView)
     }
 }
