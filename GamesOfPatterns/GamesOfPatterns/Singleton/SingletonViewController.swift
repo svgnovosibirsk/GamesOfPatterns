65aaffe80eafa616903d2a41de0adb3c9fc375eb
Sergey <>
2023-09-28 11:41:51 +0400

    add buttons

--- GamesOfPatterns/GamesOfPatterns/Singleton/SingletonViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/Singleton/SingletonViewController.swift
@@ -12,6 +12,9 @@ final class SingletonViewController: UIViewController {
     let backgroundView = UIImageView(frame: .zero)
     let stackView = UIStackView(frame: .zero)
     let textView = UITextView(frame: .zero)
+    // TODO: password text field
+    let saveButton = UIButton(frame: .zero)
+    let readButton = UIButton(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,10 @@ final class SingletonViewController: UIViewController {
     private func setupScreen() {
         setupBackgroundView()
         setupStackView()
+        setupTextView()
+        // TODO: password text field
+        setupSaveButton()
+        setupReadButton()
     }
     
     // MARK: Background
@@ -48,7 +55,6 @@ final class SingletonViewController: UIViewController {
     
     // MARK: Stackview
     private func setupStackView() {
-        //stackView.backgroundColor = .white
         stackView.axis = .vertical
         stackView.alignment = .center
         stackView.distribution = .equalSpacing
@@ -67,5 +73,65 @@ final class SingletonViewController: UIViewController {
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    // MARK: Text View
+    private func setupTextView() {
+        textView.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        textView.layer.cornerRadius = 10
+        textView.textColor = .white
+        textView.font = UIFont.systemFont(ofSize: 20)
+        
+        setupTextViewConstraints()
+    }
+    
+    private func setupTextViewConstraints() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(textView)
+        
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 30),
+            textView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 50),
+            textView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant:  -30),
+            textView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    // TODO: password text field
+    
+    // MARK: Save Button
+    private func setupSaveButton() {
+        saveButton.setTitle("     Save     ", for: .normal)
+        saveButton.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        saveButton.layer.cornerRadius = 10
+        saveButton.setTitleColor(.systemGray6, for: .normal)
+        saveButton.setTitleColor(.systemGray4, for: .highlighted)
+        saveButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30.0)
+        saveButton.addTarget(self, action: #selector(saveButtonPressed), for: .touchUpInside)
+        
+        stackView.addArrangedSubview(saveButton)
+    }
+    
+    @objc private func saveButtonPressed(_ sender: UIButton) {
+        //viewModel?.cloneButtonPressed()
+        print(#function)
+    }
+    
+    // MARK: Read Button
+    private func setupReadButton() {
+        readButton.setTitle("     Read     ", for: .normal)
+        readButton.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        readButton.layer.cornerRadius = 10
+        readButton.setTitleColor(.systemGray6, for: .normal)
+        readButton.setTitleColor(.systemGray4, for: .highlighted)
+        readButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30.0)
+        readButton.addTarget(self, action: #selector(readButtonPressed), for: .touchUpInside)
+        
+        stackView.addArrangedSubview(readButton)
+    }
+    
+    @objc private func readButtonPressed(_ sender: UIButton) {
+        //viewModel?.cloneButtonPressed()
+        print(#function)
     }
 }
