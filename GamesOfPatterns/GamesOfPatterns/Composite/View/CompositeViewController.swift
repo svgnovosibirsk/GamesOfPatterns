31369acff516775d11d7b0a4b3c8f72e75f86615
Sergey <>
2023-12-30 13:11:18 +0400

    add buttons and label

--- GamesOfPatterns/GamesOfPatterns/Composite/View/CompositeViewController.swift
+++ GamesOfPatterns/GamesOfPatterns/Composite/View/CompositeViewController.swift
@@ -11,18 +11,46 @@ class CompositeViewController: UIViewController {
     
     let logoImageView: UIImageView = {
         let imageView = UIImageView(image: ImagesProvider.wcLogo)
-        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let backgroundImageView: UIImageView = {
         let imageView = UIImageView(image: ImagesProvider.wcBackground)
-        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        imageView.contentMode = .scaleAspectFit
         return imageView
+    }()
+    
+    let orcButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Orcs", for: .normal)
+        btn.setTitleColor(.systemGreen, for: .normal)
+        btn.setTitleColor(.systemGray, for: .highlighted)
+        btn.addTarget(self, action: #selector(orcButtonPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    let humanButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Humans", for: .normal)
+        btn.setTitleColor(.systemBlue, for: .normal)
+        btn.setTitleColor(.systemGray, for: .highlighted)
+        btn.addTarget(self, action: #selector(humanButtonPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.textColor = .systemBlue
+        return label
     }()
 
     override func viewDidLoad() {
@@ -32,6 +60,14 @@ class CompositeViewController: UIViewController {
 
         setupUI()
     }
+    
+    @objc func orcButtonPressed() {
+        print(#function)
+    }
+    
+    @objc func humanButtonPressed() {
+        print(#function)
+    }
 
 }
 
@@ -39,6 +75,9 @@ private extension CompositeViewController {
     func setupUI() {
         setupLogo()
         setupBackground()
+        setupOrcButton()
+        setupHumanButton()
+        setupNumberLabel()
     }
     
     func setupLogo() {
@@ -56,6 +95,33 @@ private extension CompositeViewController {
         NSLayoutConstraint.activate([
             backgroundImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
             backgroundImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func setupOrcButton() {
+        view.addSubview(orcButton)
+        
+        NSLayoutConstraint.activate([
+            orcButton.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: 10),
+            orcButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -80)
+        ])
+    }
+    
+    func setupHumanButton() {
+        view.addSubview(humanButton)
+        
+        NSLayoutConstraint.activate([
+            humanButton.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: 10),
+            humanButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 80)
+        ])
+    }
+    
+    func setupNumberLabel() {
+        view.addSubview(numberLabel)
+        
+        NSLayoutConstraint.activate([
+            numberLabel.topAnchor.constraint(equalTo: orcButton.bottomAnchor, constant: 10),
+            numberLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 }
