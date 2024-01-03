262dea3012c2be515fb2650ee1fd9643351f078c
Sergey <>
2024-01-03 11:58:17 +0400

    implement async version GCD

--- GamesOfPatterns/GamesOfPatterns/Facade/FacadeModel.swift
+++ GamesOfPatterns/GamesOfPatterns/Facade/FacadeModel.swift
@@ -12,14 +12,14 @@ final class FacadeModel {
     // MARK: - Facade
 //    func startStarOfDeath() -> String {
 //        var readySign = ""
-//
+//        
 //        let starNuclearFuelSystem = StarNuclearFuelSystem()
 //        let starEngine =  StarEngine()
 //        let starWeapons = StarWeapons()
 //        let starNavigation = StarNavigation()
 //        let starOxigenSystem = StarOxigenSystem()
 //        let starElectricSystem = StarElectricSystem()
-//
+//        
 //        let fuel = starNuclearFuelSystem.getNuclearFuel()
 //        starEngine.startEngine(with: fuel)
 //        starElectricSystem.startElectricSystem(with: fuel)
@@ -27,15 +27,105 @@ final class FacadeModel {
 //        starOxigenSystem.startOxigenSystem()
 //        starWeapons.prepareWeapon()
 //        starNavigation.calculateTransition()
-//
-//
+//        
+//        
 //        print("Star of Death is Ready")
-//
+//        
 //        readySign = "READY"
-//
+//        
 //        return readySign
 //    }
     
+    //MARK: - Async version 1
+    //    func startStarOfDeath(completion: @escaping (String) -> Void) {
+    //        var readySign = ""
+    //
+    //        let starNuclearFuelSystem = StarNuclearFuelSystem()
+    //        let starEngine =  StarEngine()
+    //        let starWeapons = StarWeapons()
+    //        let starNavigation = StarNavigation()
+    //        let starOxigenSystem = StarOxigenSystem()
+    //        let starElectricSystem = StarElectricSystem()
+    //
+    //        let startGroup = DispatchGroup()
+    //        let startQueue = DispatchQueue(label: "com.star-start.queue", qos: .utility)
+    //
+    //        var fuel = NuclearFuel(energy: 0)
+    //
+    //
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            fuel = starNuclearFuelSystem.getNuclearFuel()
+    //            startGroup.leave()
+    //        }
+    //
+    //        //TODO: bind with getNuclearFuel
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            starEngine.startEngine(with: fuel)
+    //            startGroup.leave()
+    //        }
+    //
+    //        //TODO: bind with getNuclearFuel
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            starElectricSystem.startElectricSystem(with: fuel)
+    //            startGroup.leave()
+    //        }
+    //
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            //TODO: make checkOxigenSystem return true or false?
+    //            starOxigenSystem.checkOxigenSystem()
+    //            startGroup.leave()
+    //        }
+    //
+    //        //TODO: bind with checkOxigenSystem
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            starOxigenSystem.startOxigenSystem()
+    //            startGroup.leave()
+    //        }
+    //
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            starWeapons.prepareWeapon()
+    //            startGroup.leave()
+    //        }
+    //
+    //
+    //        startQueue.async {
+    //            startGroup.enter()
+    //            starNavigation.calculateTransition()
+    //            startGroup.leave()
+    //        }
+    //
+    //        startGroup.notify(queue: startQueue) {
+    //            DispatchQueue.main.async {
+    //                print("Star of Death is Ready")
+    //            }
+    //            readySign = "READY"
+    //            completion(readySign)
+    //        }
+    //
+    //
+    //        //let fuel = starNuclearFuelSystem.getNuclearFuel()
+    //        //starEngine.startEngine(with: fuel)
+    //        //starElectricSystem.startElectricSystem(with: fuel)
+    //        //starOxigenSystem.checkOxigenSystem()
+    //        //starOxigenSystem.startOxigenSystem()
+    //        //starWeapons.prepareWeapon()
+    //        //starNavigation.calculateTransition()
+    //
+    //
+    ////        print("Star of Death is Ready")
+    ////
+    ////        readySign = "READY"
+    //        //return readySign
+    //    }
+    //}
+    
+    //MARK: - Async version GCD
     func startStarOfDeath(completion: @escaping (String) -> Void) {
         var readySign = ""
         
@@ -47,56 +137,52 @@ final class FacadeModel {
         let starElectricSystem = StarElectricSystem()
         
         let startGroup = DispatchGroup()
-        let startQueue = DispatchQueue(label: "com.star-start.queue", qos: .utility)
+        let startQueue = DispatchQueue(label: "com.star-start.queue", qos: .utility, attributes: .concurrent)
         
         var fuel = NuclearFuel(energy: 0)
         
-        
-        startQueue.async {
+        startQueue.sync {
             startGroup.enter()
-            fuel = starNuclearFuelSystem.getNuclearFuel()
-            startGroup.leave()
-        }
-        
-        //TODO: bind with getNuclearFuel
-        startQueue.async {
+            startQueue.sync {
+                fuel = starNuclearFuelSystem.getNuclearFuel()
+                startGroup.leave()
+            }
+            
             startGroup.enter()
-            starEngine.startEngine(with: fuel)
-            startGroup.leave()
-        }
-        
-        //TODO: bind with getNuclearFuel
-        startQueue.async {
+            startQueue.async {
+                starEngine.startEngine(with: fuel)
+                startGroup.leave()
+            }
+            
             startGroup.enter()
-            starElectricSystem.startElectricSystem(with: fuel)
-            startGroup.leave()
-        }
-        
-        startQueue.async {
+            startQueue.async {
+                starElectricSystem.startElectricSystem(with: fuel)
+                startGroup.leave()
+            }
+            
             startGroup.enter()
-            //TODO: make checkOxigenSystem return true or false?
-            starOxigenSystem.checkOxigenSystem()
-            startGroup.leave()
-        }
-        
-        //TODO: bind with checkOxigenSystem
-        startQueue.async {
+            startQueue.async {
+                starOxigenSystem.checkOxigenSystem()
+                startGroup.leave()
+                
+                startGroup.enter()
+                startQueue.sync {
+                    starOxigenSystem.startOxigenSystem()
+                    startGroup.leave()
+                }
+            }
+            
             startGroup.enter()
-            starOxigenSystem.startOxigenSystem()
-            startGroup.leave()
-        }
-        
-        startQueue.async {
+            startQueue.async {
+                starWeapons.prepareWeapon()
+                startGroup.leave()
+            }
+            
             startGroup.enter()
-            starWeapons.prepareWeapon()
-            startGroup.leave()
-        }
-        
-        
-        startQueue.async {
-            startGroup.enter()
-            starNavigation.calculateTransition()
-            startGroup.leave()
+            startQueue.async {
+                starNavigation.calculateTransition()
+                startGroup.leave()
+            }
         }
         
         startGroup.notify(queue: startQueue) {
@@ -106,21 +192,6 @@ final class FacadeModel {
             readySign = "READY"
             completion(readySign)
         }
-        
-        
-        //let fuel = starNuclearFuelSystem.getNuclearFuel()
-        //starEngine.startEngine(with: fuel)
-        //starElectricSystem.startElectricSystem(with: fuel)
-        //starOxigenSystem.checkOxigenSystem()
-        //starOxigenSystem.startOxigenSystem()
-        //starWeapons.prepareWeapon()
-        //starNavigation.calculateTransition()
-        
-        
-//        print("Star of Death is Ready")
-//
-//        readySign = "READY"
-        //return readySign
     }
 }
 
@@ -139,6 +210,7 @@ struct NuclearFuel {
 
 final class StarNuclearFuelSystem {
     func getNuclearFuel() -> NuclearFuel {
+        print(#function)
         sleep(UInt32.random(in: 1...3))
         return NuclearFuel(energy: 1_000_000_000_000)
     }
